9b24cb61fdb67030dc0a8f650d2f7450c8abd08e
Matías B Crespo <matisfd@gmail.com>
2021-03-21 07:42:14 -0300

    ej 05

---
+++ laboratorio_3/clase01/guia/ej05/ej05.js
@@ -0,0 +1,22 @@
+"use strict";
+var nombre = "BENITO";
+var apellido = "FERNANDEZ";
+var nombreApellido = function (nombre, apellido) {
+    var contador = 0;
+    var nombreMinuscula = "";
+    for (var _i = 0, nombre_1 = nombre; _i < nombre_1.length; _i++) {
+        var iterator = nombre_1[_i];
+        if (contador > 0 && contador < nombre.length) {
+            nombreMinuscula += nombre[contador];
+        }
+        contador++;
+    }
+    console.log("NOMBRE MINUSCULA " + nombreMinuscula + "\n");
+    var str = "";
+    if (nombre && apellido) {
+        str += apellido.toUpperCase() + ", " + nombre[0].toUpperCase() + nombreMinuscula.toLocaleLowerCase() + ".";
+    }
+    console.log(str);
+};
+nombreApellido(nombre, apellido);
+//# sourceMappingURL=ej05.js.map
--- laboratorio_3/clase01/guia/ej05/ej05.ts
+++ laboratorio_3/clase01/guia/ej05/ej05.ts
@@ -1,7 +1,28 @@
+
+let nombre:string="BENITO";
+let apellido:string="FERNANDEZ";
+
 let nombreApellido:Function = (nombre:string,apellido:string):void=>
 {
-    let str:string="";
+    var contador=0;
+    var nombreMinuscula:string="";
+    for (let iterator of nombre)
+    {
+        if(contador>0&&contador<nombre.length)
+        {
+            nombreMinuscula+=nombre[contador];
+        }
+        contador++;        
+    }
+    console.log("NOMBRE MINUSCULA "+nombreMinuscula+"\n");
 
+    let str:string="";
+    if(nombre && apellido)
+    {        
+        str+=`${apellido.toUpperCase()}, ${nombre[0].toUpperCase()}${nombreMinuscula.toLocaleLowerCase()}.`
+    }
     
-    
+    console.log(str);    
 } 
+
+nombreApellido(nombre,apellido);
