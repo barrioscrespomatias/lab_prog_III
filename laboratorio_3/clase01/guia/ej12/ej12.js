c10cfe80b47c12265557c1e924ae530faf062cab
Matías B Crespo <matisfd@gmail.com>
2021-03-25 00:09:28 -0300

    Ejercicio 12

--- laboratorio_3/clase01/guia/ej12/ej12.js
+++ laboratorio_3/clase01/guia/ej12/ej12.js
@@ -1,51 +1,40 @@
 "use strict";
-var fecha = "22-08-1990";
+var fecha = "22-07-1990";
 var obtenerSigno = function (cadena) {
     var strSalida = "";
     var dia = 0;
     var mes = 0;
     var anio = 0;
-    var fechaDate;
     var signos = Array("Capricornio", "Acuario", "Piscis", "Aries", "Tauro", "Géminis", "Cáncer", "Leo", "Virgo", "Libra", "Escorpio", "Sagitario");
     var diaMesAnio = cadena.split("-", 3);
     dia = parseInt(diaMesAnio[0]);
     mes = parseInt(diaMesAnio[1]);
-    anio = parseInt(diaMesAnio[2]);
-    //Resolución simple -> NO ES EFECTIVA.
+    //20
+    if (mes == 2) {
+        if (dia < 20)
+            mes -= 1;
+    }
     //21
-    if (mes > 1 && mes < 5 && mes == 0) {
+    if (mes > 2 && mes < 6 || mes == 1) {
         if (dia < 21)
-            strSalida = signos[mes - 1];
-        else
-            strSalida = signos[mes];
+            mes -= 1;
     }
     //22
-    else if (mes == 5 || mes == 11) {
+    else if (mes == 6 || mes == 12) {
         if (dia < 22)
-            strSalida = signos[mes - 1];
-        else
-            strSalida = signos[mes];
+            mes -= 1;
     }
     //23
-    else if (mes == 6 || mes > 7 && mes < 11) {
+    else if (mes == 7 || mes > 8 && mes < 12) {
         if (dia < 23)
-            strSalida = signos[mes - 1];
-        else
-            strSalida = signos[mes];
+            mes -= 1;
     }
     //24
-    else if (mes == 7) {
+    else if (mes == 8) {
         if (dia < 24)
-            strSalida = signos[mes - 1];
-        else
-            strSalida = signos[mes];
+            mes -= 1;
     }
-    else {
-        if (dia < 20)
-            strSalida = signos[mes - 1];
-        else
-            strSalida = signos[mes];
-    }
+    strSalida = signos[mes];
     /* #region  CODIGO FEO */
     //Resolución con muchos if else.
     // if(fechaDate.getMonth() == 0)//enero
