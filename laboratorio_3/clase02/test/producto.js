80fa31071e601e5e783d38a872dd4c312b66dfc7
Matías B Crespo <matisfd@gmail.com>
2021-03-27 20:02:44 -0300

    Ejercicios laboratorio clase 02

---
+++ laboratorio_3/clase02/test/producto.js
@@ -0,0 +1,6 @@
+"use strict";
+function datosCompra() {
+    var precio = document.getElementById("chTermo").value;
+    alert("El precio es " + precio);
+}
+//# sourceMappingURL=producto.js.map
---
+++ laboratorio_3/clase02/test/producto.ts
@@ -0,0 +1,9 @@
+
+
+function datosCompra():void
+{
+    let precio: string = (<HTMLInputElement>document.getElementById("chTermo")).value;
+    alert(`El precio es ${precio}`);   
+
+}
+
