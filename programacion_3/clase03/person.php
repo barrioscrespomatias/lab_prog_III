f6f39050acb2452f1303a4913a34b4de54d908b3
Matías B Crespo <matisfd@gmail.com>
2021-03-30 14:49:06 -0300

    Ejercicios con archivos

--- programacion_3/clase03/person.php
+++ programacion_3/clase03/person.php
@@ -11,22 +11,7 @@ class Persona
         $this->legajo = $legajo;
         $this->apellido = $apellido;
         $this->nombre = $nombre;       
-    }
-
-    public function buscarPorId(Persona $persona, $id):string
-    {
-        $strInfo = "";
-        if($persona != null && $id != null)
-        {
-            if($persona->legajo == $id) 
-            {
-                $strInfo .= "Legajo: " . $persona->legajo;
-                $strInfo .= "Nombre: " . $persona->nombre;
-                $strInfo .= "Apellido: " . $persona->apellido;
-            }
-        }
-        return $strInfo;
-    }
+    }    
 
     public function ToString()
     {
