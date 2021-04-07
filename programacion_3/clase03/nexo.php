3b69cbd98618794a6ed1eec2cfa9a6d1cb7e4d29
Matías B Crespo <matisfd@gmail.com>
2021-04-07 20:23:09 -0300

    Ejercicio 40 -> Se debe mejorar

--- programacion_3/clase03/nexo.php
+++ programacion_3/clase03/nexo.php
@@ -10,19 +10,19 @@ $apellido = isset($_POST["apellido"]) ? $_POST["apellido"] : "Error";
 $legajo = isset($_POST["legajo"]) ? $_POST["legajo"] : "Error";
 
 //Confirmo login.
-if(strlen($nombre)>0)
+if (strlen($nombre) > 0)
     $_SESSION['nombre'] = $nombre;
+
 
 
 //IMAGENES
 
-if (isset($_FILES["foto"]["name"])) {    
-    
+if (isset($_FILES["foto"]["name"])) {
+
     $array = explode(".", $_FILES["foto"]["name"]);
     $extension = end($array);
     $direccionImg = './fotos/' . $legajo . '_' . $nombre . "." . $extension;
     $cadena = $legajo . " - " . $apellido . " - " . $nombre . " - " . $direccionImg . "\n";
-
 }
 
 $path = "./archivos/alumnos.txt";
@@ -30,10 +30,10 @@ $path = "./archivos/alumnos.txt";
 switch ($accion) {
     case "alta":
 
-        $login = isset($_SESSION['nombre']) ? $_SESSION['nombre'] : "Error";        
+        $login = isset($_SESSION['nombre']) ? $_SESSION['nombre'] : "Error";
         if ($login != "Error") {
 
-            echo "Bienvenido ".$login."!!";
+            echo "Bienvenido " . $login . "!!";
             $archivo = fopen($path, "a");
             fwrite($archivo, $cadena);
 
@@ -41,14 +41,13 @@ switch ($accion) {
             move_uploaded_file($_FILES["foto"]["tmp_name"], $direccionImg);
             echo "Se ha guardado correctamente el registro de ${nombre}";
             session_unset();
-            ?>
+?>
             <a href="index.html"><br>Ir a la pagina principal</a>
-            <?php
+        <?php
             fclose($archivo);
-        }
-        else
-        header('location: index.html');
-        
+        } else
+            header('location: index.html');
+
         break;
     case "listado":
         $archivo = fopen($path, "r");
@@ -63,26 +62,29 @@ switch ($accion) {
 
             $renglon = fgets($archivo);
             $persona = explode(" - ", $renglon);
-            
+
             //Legajo apellido nombre path
             if ($persona[0] == $legajo) {
                 $_SESSION["legajo"] = $persona[0];
                 $_SESSION["apellido"] = $persona[1];
                 $_SESSION["nombre"] = $persona[2];
-                $_SESSION["path"] = $persona[3];                          
+                $_SESSION["path"] = $persona[3];
                 break;
             }
         }
+
 
         foreach ($persona as $item) {
             echo $item . " ";
         }
 
         ?>
-                <a href="principal.php"><h2>Ir a pagina principal</h2></a>                
-        <?php  
-        
 
+        <a href="principal.php">
+            <h2>Ir a pagina principal</h2>
+        </a>
+
+        <?php
         fclose($archivo);
         break;
 }
